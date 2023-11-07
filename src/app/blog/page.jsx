37b5b40aafb5e6401bd4606b3386db66d429f94c
Rajib Chandra Das <prorajib6@gmail.com>
2023-11-07 22:04:52 +0600

    portfolio page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import styles from './page.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const blog = () => {
-  return <div className={styles.container}>blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href='/testId' className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/10416213/pexels-photo-10416213.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=''
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Blog Title</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            aperiam ut debitis numquam quo dignissimos aliquam commodi eius
+            reprehenderit, maxime quam, facilis beatae placeat non perspiciatis
+            ex sint inventore voluptate.
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default blog;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import styles from './page.module.css';
+import Link from 'next/link';
 const portfolio = () => {
-  return <div className={styles.container}>portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link className={styles.item} href='/portfolio/illustrations'>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link className={styles.item} href='/portfolio/websites'>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link className={styles.item} href='/portfolio/appliction'>
+          <span className={styles.title}>Application</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default portfolio;
